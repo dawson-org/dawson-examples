9712e1ef94be4f348c6a15988e70dac99f35de3b
Simone Lusenti <simone@plasticpanda.com>
2017-03-07 17:45:37 +0100

    adds basic python example

---
+++ 2-basic-python/api.py
@@ -0,0 +1,44 @@
+""" A module with two dawson-valid Python functions """
+
+import json
+from countrycode import countrycode
+
+def myfunction(event, context):
+    """ This function is dawson-valid """
+    print(event, context)
+    return "Hello Function"
+
+myfunction.api = {
+    "path": "test"
+}
+
+def myfunction2(event, context):
+    """ This function is dawson-valid """
+    print(event, context)
+    return "Hello world 2"
+
+myfunction2.api = {
+    "path": "test2"
+}
+
+def myfunction3(event, context):
+    """ This function is dawson-valid """
+    print(event, context)
+    cc = countrycode(codes=['Algeria', 'United States'], origin='country_name', target='iso3c')
+    return ','.join(cc)
+
+myfunction3.api = {
+    "path": "test3"
+}
+
+def myfunctionWithError(event, context):
+    """ This function is dawson-valid """
+    print(event, context)
+    raise Exception(json.dumps({
+      'httpStatus': 500,
+      'response': 'This function failed'
+    }))
+
+myfunctionWithError.api = {
+    "path": "testError"
+}
