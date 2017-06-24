7999ebdaab27cc5054a05dc4ad66683c7226e25e
Simone Lusenti <lusenti.s@gmail.com>
2017-06-24 12:13:06 +0200

    feat: add index method and rename greet

--- 0-basic-srp-pug/api.js
+++ 0-basic-srp-pug/api.js
@@ -2,11 +2,20 @@ import path from 'path';
 import pug from 'pug';
 const template = pug.compileFile(path.join(__dirname, 'template.pug'));
 
-export function homepage (event) {
+export function greet (event) {
   return template({
     name: event.params.path.name
   });
 }
-homepage.api = {
+greet.api = {
   path: 'hello/{name}'
+};
+
+export function index (event) {
+  return `
+  It worked! Navigate to /hello/<your name> :)
+`;
+}
+index.api = {
+  path: ''
 };
